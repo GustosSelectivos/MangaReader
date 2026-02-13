6c17bf85536440712bf110aa99dc7a5b195ffa04
GustosSelectivos <gojhangames@gmail.com>
2026-02-13 13:27:26 -0300

    feat: initialize models package by importing all defined models.

--- MangaApi/ApiCore/models/__init__.py
+++ MangaApi/ApiCore/models/__init__.py
@@ -1,5 +1,5 @@
 from .user_models import UserProfile
-from .manga_models import manga, Tag, Author
-from .chapter_models import Chapter, ChapterImage
-from .mantenedor_models import Demografia, Estado, Profile, Idioma
+from .manga_models import manga, manga_alt_titulo, manga_cover, manga_autor, manga_tag
+from .chapter_models import chapter
+from .mantenedor_models import demografia, estados, autores, tags
 from .dac_models import Permission, AccessGrant, Owner, AuditLog
