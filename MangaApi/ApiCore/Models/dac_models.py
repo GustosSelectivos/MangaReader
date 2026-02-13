6c1ea76df6834c8446619b0457e37a8932693cf5
GustosSelectivos <gojhangames@gmail.com>
2026-02-13 12:57:17 -0300

    feat: Add Discretionary Access Control (DAC) models including Permission, AccessGrant, Owner, and AuditLog.

--- MangaApi/ApiCore/Models/dac_models.py
+++ MangaApi/ApiCore/Models/dac_models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import Group
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 
+
 User = get_user_model()
 
 
