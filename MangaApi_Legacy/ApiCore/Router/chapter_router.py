de3c9bf456d71cf5d958bd541213d42f7bfe10cf
GustosSelectivos <gojhangames@gmail.com>
2026-06-16 21:22:18 -0400

    refactor: remove unused project configuration files

--- MangaApi_Legacy/ApiCore/Router/chapter_router.py
+++
@@ -1,5 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from ApiCore.View.chapter_view import ChapterViewSet
-
-router = DefaultRouter()
-router.register(r'', ChapterViewSet, basename='chapters')
--- MangaApi_Legacy/ApiCore/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from .user_models import UserProfile
-from .manga_models import manga, manga_alt_titulo, manga_cover, manga_autor, manga_tag
-from .chapter_models import chapter
-from .mantenedor_models import demografia, estados, autores, tags
-from .dac_models import Permission, AccessGrant, Owner, AuditLog
